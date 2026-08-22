f0a4a81ee97a4503353a3f10ba168285a1a8d14a
Anderson Souza <andersonsouzac06@gmail.com>
2026-08-21 23:58:29 -0300

    fix: corrige config de imagem no Render e adiciona team_slug no Netlify

--- terraform/landing.tf
+++ terraform/landing.tf
@@ -1,6 +1,7 @@
 # Busca a referência do site da Landing Page na Netlify
 data "netlify_site" "landing" {
   name = var.netlify_site_name
+  team_slug = "andersonsouzac06"
 }
 
 # Configura a variável de ambiente PUBLIC_APP_URL com a URL gerada para o Frontend (Render)
